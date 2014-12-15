7328602964145e4d1c14e3ce088c6497f166d644
Étienne Vallette d'Osia <dohzya@gmail.com>
2014-12-15 18:53:40 +0900

    Fix ops now reset bi

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func isOp(ctx context, c byte) bool {
 
 // Critic converts critic markup into HTML
 func Critic(w io.Writer, r io.Reader) (int, error) {
-	rbuf := make([]byte, 16) // actual buffer
+	rbuf := make([]byte, 32) // actual buffer
 	buf := rbuf[2:]          // buf used for reading
 	read := 0                // total bytes read
 	bi := 2                  // index of 1st byte of rbuf which is a data
@@ -118,6 +118,7 @@ main: // main iteration (1 loop = 1 read)
 				// the <ins> tag will be writen after having read all
 				// `\n` following the `{++` tag.
 				offset += 3
+				bi = 2
 			case "++}":
 				var s string
 				if !ctx.insTagged {
@@ -136,54 +137,63 @@ main: // main iteration (1 loop = 1 read)
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "{--":
 				ctx.mode = mDel
 				if _, err := w.Write([]byte("<del>")); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "--}":
 				ctx.mode = mNormal
 				if _, err := w.Write([]byte("</del>")); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "{~~":
 				ctx.mode = mSub
 				if _, err := w.Write([]byte("<del>")); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "~~}":
 				ctx.mode = mNormal
 				if _, err := w.Write([]byte("</ins>")); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "{==":
 				ctx.mode = mHighligh
 				if _, err := w.Write([]byte("<mark>")); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "==}":
 				ctx.mode = mNormal
 				if _, err := w.Write([]byte("</mark>")); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "{>>":
 				ctx.mode = mComment
 				if _, err := w.Write([]byte(`<span class="critic comment">`)); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			case "<<}":
 				ctx.mode = mNormal
 				if _, err := w.Write([]byte(`</span>`)); err != nil {
 					return read, err
 				}
 				offset += 3
+				bi = 2
 			default:
 				if ctx.mode == mIns && !ctx.insTagged {
 					if _, err := w.Write([]byte("<ins>")); err != nil {
@@ -196,89 +206,80 @@ main: // main iteration (1 loop = 1 read)
 						return read, err
 					}
 					offset += 2
+					bi = 2
 					continue sub
 				}
 				if _, err := w.Write(data[offset : offset+1]); err != nil {
 					return read, err
 				}
 				offset++
+				bi = 2
 				continue sub
 			}
 		}
 	}
 }
 
-func ex1(ext int) {
-	critic := `lacus{++ est++} Pra{e}sent.`
-	exp := `<p>lacus<ins> est</ins> Pra{e}sent.</p>
-`
+func ex(num int, critic string, exp string) {
 	md := bytes.NewBuffer(make([]byte, 0))
 	_, err := Critic(md, bytes.NewBufferString(critic))
 	if err != nil {
 		fmt.Printf("failed: %s\n", err.Error())
 		return
 	}
-	readb := blackfriday.MarkdownHtml(md.Bytes(), ext)
+	readb := blackfriday.MarkdownHtml(md.Bytes(), blackfriday.CommonExtensions)
 	real := string(readb)
-	fmt.Printf("critic  : ---%s---\n", critic)
-	fmt.Printf("md      : ---%s---\n", md)
-	// fmt.Printf("real    : ---%v---\n", real[:len(real)-1])
-	// fmt.Printf("expected: ---%v---\n", exp[:len(exp)-1])
-	fmt.Printf("\n%v\n", real == exp)
+	ok := real == exp
+	if !ok {
+		fmt.Printf("[%d] critic  : ---%s---\n", num, critic)
+		fmt.Printf("[%d] md      : ---%s---\n", num, md)
+		fmt.Printf("[%d] real    : ---%v---\n", num, real[:len(real)-1])
+		fmt.Printf("[%d] expected: ---%v---\n", num, exp[:len(exp)-1])
+	}
+	fmt.Printf("[%d] %v\n", num, ok)
 }
 
-func ex2(ext int) {
-	critic := `Don't go around saying{-- to people that--} the world owes you
+func main() {
+	ex(
+		1,
+		`lacus{++ est++} Pra{e}sent.`,
+		`<p>lacus<ins> est</ins> Pra{e}sent.</p>
+`,
+	)
+
+	ex(
+		2,
+		`Don't go around saying{-- to people that--} the world owes you
 a living. The world owes you nothing. It was here first. {~~One~>Only one~~}
 thing is impossible for God: To find {++any++} sense in any copyright law
 on the planet. {==Truth is stranger than fiction==}{>>strange but true<<},
-but it is because Fiction is obliged to stick to possibilities; Truth isn't.`
-	exp := `<p>Don't go around saying<del> to people that</del> the world owes you
+but it is because Fiction is obliged to stick to possibilities; Truth isn't.`,
+		`<p>Don't go around saying<del> to people that</del> the world owes you
 a living. The world owes you nothing. It was here first. <del>One</del><ins>Only one</ins>
 thing is impossible for God: To find <ins>any</ins> sense in any copyright law
 on the planet. <mark>Truth is stranger than fiction</mark><span class="critic comment">strange but true</span>,
 but it is because Fiction is obliged to stick to possibilities; Truth isn't.</p>
-`
-	md := bytes.NewBuffer(make([]byte, 0))
-	_, err := Critic(md, bytes.NewBufferString(critic))
-	if err != nil {
-		fmt.Printf("failed: %s\n", err.Error())
-		return
-	}
-	readb := blackfriday.MarkdownHtml(md.Bytes(), ext)
-	real := string(readb)
-	fmt.Printf("critic  : ---%s---\n", critic)
-	fmt.Printf("md      : ---%s---\n", md)
-	// fmt.Printf("real    : ---%v---\n", real[:len(real)-1])
-	// fmt.Printf("expected: ---%v---\n", exp[:len(exp)-1])
-	fmt.Printf("\n%v\n", real == exp)
-}
+`,
+	)
 
-func ex3(ext int) {
-	critic := `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at orci magna. Phasellus augue justo, sodales eu pulvinar ac, vulputate eget nulla. Mauris massa sem, tempor sed cursus et, semper tincidunt lacus.{++
-++}Praesent sagittis, quam id egestas consequat, nisl orci vehicula libero, quis ultricies nulla magna interdum sem. Maecenas eget orci vitae eros accumsan mollis. Cras mi mi, rutrum id aliquam in, aliquet vitae tellus. Sed neque justo, cursus in commodo eget, facilisis eget nunc. Cras tincidunt auctor varius.`
-	exp := `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at orci magna. Phasellus augue justo, sodales eu pulvinar ac, vulputate eget nulla. Mauris massa sem, tempor sed cursus et, semper tincidunt lacus.
-<ins class="break">&nbsp;</ins>
-Praesent sagittis, quam id egestas consequat, nisl orci vehicula libero, quis ultricies nulla magna interdum sem. Maecenas eget orci vitae eros accumsan mollis. Cras mi mi, rutrum id aliquam in, aliquet vitae tellus. Sed neque justo, cursus in commodo eget, facilisis eget nunc. Cras tincidunt auctor varius.</p>
-`
-	md := bytes.NewBuffer(make([]byte, 0))
-	_, err := Critic(md, bytes.NewBufferString(critic))
-	if err != nil {
-		fmt.Printf("failed: %s\n", err.Error())
-		return
-	}
-	readb := blackfriday.MarkdownHtml(md.Bytes(), ext)
-	real := string(readb)
-	fmt.Printf("critic  : ---%s---\n", critic)
-	fmt.Printf("md      : ---%s---\n", md)
-	// fmt.Printf("real    : ---%v---\n", real[:len(real)-1])
-	// fmt.Printf("expected: ---%v---\n", exp[:len(exp)-1])
-	fmt.Printf("\n%v\n", real == exp)
-}
-
-func main() {
-	ext := blackfriday.CommonExtensions // | blackfriday.EXTENSION_CRITIC
-	ex1(ext)
-	ex2(ext)
-	ex3(ext)
+	// 	ex(
+	// 		3,
+	// 		`Lorem ipsum dolor sit amet, consectetur adipiscing elit.{++
+	// ++}Vestibulum at orci magna. Phasellus augue justo, sodales eu pulvinar ac,
+	// vulputate eget nulla. Mauris massa sem, tempor sed cursus et, semper tincidunt
+	// lacus. Praesent sagittis, quam id egestas consequat, nisl orci vehicula{--
+	// --}libero, quis ultricies nulla magna interdum sem. Maecenas eget orci vitae
+	// eros accumsan mollis. Cras mi mi, rutrum id aliquam in, {~~aliquet vitae~>
+	// ~~}tellus. Sed neque justo, cursus in commodo eget, facilisis eget nunc.
+	// Cras tincidunt auctor varius.`,
+	// 		`<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	// <ins class="break">&nbsp;</ins>
+	// Vestibulum at orci magna. Phasellus augue justo, sodales eu pulvinar ac,
+	// vulputate eget nulla. Mauris massa sem, tempor sed cursus et, semper tincidunt
+	// lacus. Praesent sagittis, quam id egestas consequat, nisl orci vehicula<del>&nbsp;</del>libero, quis ultricies nulla magna interdum sem. Maecenas eget orci vitae
+	// eros accumsan mollis. Cras mi mi, rutrum id aliquam in, <del>aliquet vitae<del>
+	// <ins class="break">&nbsp;</ins>
+	// tellus. Sed neque justo, cursus in commodo eget, facilisis eget nunc.
+	// Cras tincidunt auctor varius.</p>`,
+	// 	)
 }
